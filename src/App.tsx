72ebe55465aaa021174e5baf57247909514b3cce
abeerkm <68709596+abeerkm@users.noreply.github.com>
2022-11-15 23:50:03 +0200

    epicure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Header from './layouts/Header';//<Route path="/Resturants" element={<Resturants />} />
+import Homepage from './pages/homepage';
+import Resturants from './pages/Resturants';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <BrowserRouter>
+            <Header/>
+                 <Routes>
+                  <Route>
+                  <Route index element={<Homepage />} />
+                  <Route path="/Resturants" element={<Resturants />} />
+
+                 </Route>
+                   </Routes>
+                           
+                 </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ src/pages/Resturants.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+const Resturants : React.FC<{}> =() =>{
+    return(
+        <div>Resturants</div>
+    );
+};
+export default Resturants
