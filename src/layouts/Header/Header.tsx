cb16402bb67e7a07794e1bd4d7604f1779cae38c
abeerkm <68709596+abeerkm@users.noreply.github.com>
2022-11-20 00:08:26 +0200

    new

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,31 +1,52 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import {BiSearchAlt} from "react-icons/bi";
+import {RiShoppingBag2Line} from "react-icons/ri";
+import {BsPerson} from "react-icons/bs";
 import "./Header.css";
 import logo from "../../images/about-logo@3x 1.png"
+import login from "../../images/login.png";
 
 function Header() {
-	const navRef:any = useRef();
+	const navRef = useRef<HTMLDivElement>(null);
+	const [hideimage,sethideimage]=useState(true);
+	const [hideicons,sethideicons]=useState(true);
 
 	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
+		const node = navRef.current;
+		node?.classList.toggle("responsive_nav");
+		hideimage? sethideimage(false):sethideimage(true);
 	};
 
 	return (
 		<header>
 			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
+				<ul className="menuItems">
+					<li><a href="/#">Resturants</a></li>
+					<li><a href="/#">Chefs</a></li>
+				</ul>
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
 					<FaTimes />
 				</button>
 			</nav>
-			<div className="logo"><img src={logo}/></div>
+			<div className="logo"><img className={hideimage ? "logoimage" : "hideimage"} src={logo} alt="logo"/></div>
 
 			<button className="nav-btn" onClick={showNavbar}>
 				<FaBars />
 			</button>
+			<div className="rightside">
+				<button className="search" onClick={showNavbar}>
+					<BiSearchAlt />
+				</button>
+				<button className="login" onClick={showNavbar}>
+					<BsPerson />
+				</button>				
+				<button className="bag" onClick={showNavbar}>
+					<RiShoppingBag2Line />
+				</button>
+			</div>
 		</header>
 	);
 }
